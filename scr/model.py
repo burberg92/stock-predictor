107d0bb04d1c865197456a90b14d748e1f65dc9a
your nameRodrigo <burberg92@gmail.com>
2022-11-11 17:15:33 -0600

    create a model and its endpoint

--- scr/model.py
+++ scr/model.py
@@ -25,6 +25,7 @@ def train(ticker="MSFT"):
     model = Prophet()
     model.fit(df_forecast)
 
+    #saves model data so that you do not have to retrain
     joblib.dump(model, Path(BASE_DIR).joinpath(f"{ticker}.joblib"))
 
 
@@ -55,13 +56,3 @@ def convert(prediction_list):
     return output
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Predict')
-    parser.add_argument('--ticker', type=str, default='MSFT', help='Stock Ticker')
-    parser.add_argument('--days', type=int, default=7, help='Number of days to predict')
-    args = parser.parse_args()
-    
-    train(args.ticker)
-    prediction_list = predict(ticker=args.ticker, days=args.days)
-    output = convert(prediction_list)
-    print(output)
